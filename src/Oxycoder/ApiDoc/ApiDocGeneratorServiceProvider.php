4fe9a62b7024cbab05c3d7c5cf22364607400799
Your Name <you@example.com>
2018-08-17 17:24:47 +0700

    update

--- src/Oxycoder/ApiDoc/ApiDocGeneratorServiceProvider.php
+++ src/Oxycoder/ApiDoc/ApiDocGeneratorServiceProvider.php
@@ -32,6 +32,9 @@ class ApiDocGeneratorServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        $this->app->bind(\Illuminate\Contracts\Routing\UrlGenerator::class, function ($app) {
+            return new \Laravel\Lumen\Routing\UrlGenerator($app);
+        });
         $this->app->singleton('apidoc.generate', function () {
             return new GenerateDocumentation();
         });

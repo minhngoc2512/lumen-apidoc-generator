3542c3483b0376f9ea39db788d59c983d51252b3
Your Name <you@example.com>
2018-08-22 09:27:25 +0700

    update

--- src/resources/views/partials/info.blade.php
+++ src/resources/views/partials/info.blade.php
@@ -1,6 +1,6 @@
-# Info
+{{--# Info--}}
 
-Welcome to the generated API reference.
-@if($showPostmanCollectionButton)
-[Get Postman Collection]({{ url($outputPath.'/collection.json') }})
-@endif
+{{--Welcome to the generated API reference.--}}
+{{--@if($showPostmanCollectionButton)--}}
+{{--[Get Postman Collection]({{ url($outputPath.'/collection.json') }})--}}
+{{--@endif--}}
